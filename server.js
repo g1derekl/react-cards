3e257927713ca2032726aaf921b78f66606a89cb
Derek Li <derek.li@mail.utoronto.ca>
2015-09-18 18:51:21 -0700

    WIP initialize new hand

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(5000, function () {
 
   var host = server.address().address;
   var port = server.address().port;
