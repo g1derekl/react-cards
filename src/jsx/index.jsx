58a3768aa86f8e08a5165ad7ab1efa1c08557103
Derek Li <derek.li@mail.utoronto.ca>
2016-01-24 15:17:01 -0800

    git ignore compiled js

--- src/jsx/index.jsx
+++ src/jsx/index.jsx
@@ -6,6 +6,8 @@ var Route = ReactRouter.Route;
 var Link = ReactRouter.Link;
 var browserHistory = ReactRouter.browserHistory;
 
+var foo = 'blah';
+
 var socket = require('./socket/socket.jsx');
 
 var Table = React.createClass({
