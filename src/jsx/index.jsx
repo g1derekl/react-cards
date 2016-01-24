d8ec299602d1af7bd86901bfa6f88377d74b549a
Derek Li <derek.li@mail.utoronto.ca>
2016-01-24 13:53:11 -0800

    connect client to socket

--- src/jsx/index.jsx
+++ src/jsx/index.jsx
@@ -1,6 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var socket = require('./socket/socket.jsx');
+
 var Table = React.createClass({
   render: function() {
     return <div>Hello world</div>;
