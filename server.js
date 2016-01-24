c475fc47a836f36cdb722a04e8a9b4f6f264f19c
Derek Li <derek.li@mail.utoronto.ca>
2016-01-24 14:22:27 -0800

    generate table ID from timestamp

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 var express = require('express');
 var socketIO = require('socket.io');
 var http = require('http');
+var crypto = require('crypto');
 
 var app = express();
+var hmac = crypto.createHmac('md5', 'a secret');
 
 app.config = require('./config.js')[process.env.NODE_ENV].server;
 
 app.use('/public', express.static('public'));
 
-app.get('/table/:name', function (req, res) {
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/landing.html');
+});
+
+app.get('/create-table', function(req, res) {
+
+  var tableId = hmac.update(new Date().toISOString()).digest('hex');
+
+  res.redirect('/table/' + tableId);
+});
+
+app.get('/table/:id', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
