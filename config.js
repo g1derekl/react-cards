3c94462d02832763fbcd6508410c6e3e0a5a7a5f
Derek Li <derek.li@mail.utoronto.ca>
2016-01-24 04:04:15 -0800

    loaded basic config.js file

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  production: {
+
+  },
+  development: {
+    client: {
+      host: 'http://localhost:3000'
+    },
+    server: {
+      port: 3000,
+      host: 'http://localhost:3000'
+      // connection: {}
+    }
+  }
+}
