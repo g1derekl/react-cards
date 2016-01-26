cbd535fc8a707ae4bd087df28f8a57252bdce830
Derek Li <derek.li@mail.utoronto.ca>
2016-01-26 00:41:03 -0800

    draggable card across playing surface

--- src/js/table/table.jsx
+++ src/js/table/table.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var interact = require('interact.js');
 
 var socket = require('../socket/socket.js');
 var alt = require('./alt.js');
@@ -7,10 +9,41 @@ var connection = require('./connection.js');
 var PlayerStore = alt.PlayerStore;
 var PlayerActions = alt.PlayerActions;
 
+var Card = React.createClass({
+  getInitialState: function() {
+    return {
+      x: 0,
+      y: 0
+    };
+  },
+  componentDidMount: function() {
+    interact(ReactDOM.findDOMNode(this))
+      .draggable({
+        restrict: {
+          restriction: "parent",
+          elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+        },
+        onmove: this._dragHandler
+      });
+  },
+  _transform: function() {
+    return "translate(" + this.state.x + "px, " + this.state.y + "px)";
+  },
+  _dragHandler: function(e) {
+    var x = this.state.x + e.dx;
+    var y = this.state.y + e.dy;
+
+    this.setState({x: x, y: y});
+  },
+  render: function() {
+    return <img className="card" src="/public/cards/Spade/Q.svg" style={{transform: this._transform()}} />
+  }
+});
+
 var Surface = React.createClass({
   render: function() {
     return <div className="surface">
-
+      <Card />
     </div>
   }
 });
